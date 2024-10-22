8e2c78b51f0df905a0b522df03985369d3b9ae13
Samuel-Carvalho5674 <samuelvitor124@gmail.com>
2024-10-22 15:05:07 -0300

    Aprendendo sobre Provider

---
+++ lib/models/clients.dart
@@ -0,0 +1,16 @@
+import 'package:client_control/models/client.dart';
+import 'package:flutter/material.dart';
+
+class Clients extends ChangeNotifier {
+  List<Client> clients;
+
+  Clients({
+    required this.clients
+  });
+
+  void add(Client client) {
+    clients.add(client);
+    notifyListeners(); // Essa linha notifica aoss widgets que a mudança
+    // ocorreu, é como se fosse um setState({})
+  }
+}
